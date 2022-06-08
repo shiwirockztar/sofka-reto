947ccb63920ee6bd89ab52de9dbc4b48a0d223d4
shiwirockztar <shiwirockztar@gmail.com>
2022-06-07 23:27:28 -0500

    concept two numeral

--- js/script.js
+++ js/script.js
@@ -2,20 +2,44 @@ console.log("start project");
 /*1. Precondiciones: Debe de tener 25 preguntas (5 preguntas por categorías) para 5
 rondas, cada categoría tiene una complejidad o nivel de dificultad, cada ronda debe
 asignarle un premio que el jugador va a ganar, el premio puede ser puntos o dinero.*/
-let preg1A={pregunta:"esta es la pregunta A?",opcionA: true,pre1A: "opcion A", opcionB: false,pre1A: "opcion B", opcionC: false,pre1A: "opcion C", opcionD: false,pre1A: "opcion D" },
-	preg2A={pregunta:"esta es la pregunta A?",opcionA: true,pre2A: "opcion A", opcionB: false,pre2A: "opcion B", opcionC: false,pre2A: "opcion C", opcionD: false,pre2A: "opcion D" },
-	preg3A={pregunta:"esta es la pregunta A?",opcionA: true,pre3A: "opcion A", opcionB: false,pre3A: "opcion B", opcionC: false,pre3A: "opcion C", opcionD: false,pre3A: "opcion D" },
-	preg4A={pregunta:"esta es la pregunta A?",opcionA: true,pre4A: "opcion A", opcionB: false,pre4A: "opcion B", opcionC: false,pre4A: "opcion C", opcionD: false,pre4A: "opcion D" },
-	preg5A={pregunta:"esta es la pregunta A?",opcionA: true,pre5A: "opcion A", opcionB: false,pre5A: "opcion B", opcionC: false,pre5A: "opcion C", opcionD: false,pre5A: "opcion D" };
+let preg1A={pregunta:"esta es la pregunta 1 A?",opcionA: true,pre1A: "opcion A", opcionB: false,pre1B: "opcion B", opcionC: false,pre1C: "opcion C", opcionD: false,pre1D: "opcion D" },
+	preg2A={pregunta:"esta es la pregunta 2 A?",opcionA: true,pre2A: "opcion A", opcionB: false,pre2B: "opcion B", opcionC: false,pre2C: "opcion C", opcionD: false,pre2D: "opcion D" },
+	preg3A={pregunta:"esta es la pregunta 3 A?",opcionA: true,pre3A: "opcion A", opcionB: false,pre3B: "opcion B", opcionC: false,pre3C: "opcion C", opcionD: false,pre3D: "opcion D" },
+	preg4A={pregunta:"esta es la pregunta 4 A?",opcionA: true,pre4A: "opcion A", opcionB: false,pre4B: "opcion B", opcionC: false,pre4C: "opcion C", opcionD: false,pre4D: "opcion D" },
+	preg5A={pregunta:"esta es la pregunta 5 A?",opcionA: true,pre5A: "opcion A", opcionB: false,pre5B: "opcion B", opcionC: false,pre5C: "opcion C", opcionD: false,pre5D: "opcion D" };
+
+let preg1B={pregunta:"esta es la pregunta 1 B?",opcionA: true,pre1A: "opcion A", opcionB: false,pre1B: "opcion B", opcionC: false,pre1C: "opcion C", opcionD: false,pre1D: "opcion D" },
+	preg2B={pregunta:"esta es la pregunta 2 B?",opcionA: true,pre2A: "opcion A", opcionB: false,pre2B: "opcion B", opcionC: false,pre2C: "opcion C", opcionD: false,pre2D: "opcion D" },
+	preg3B={pregunta:"esta es la pregunta 3 B?",opcionA: true,pre3A: "opcion A", opcionB: false,pre3B: "opcion B", opcionC: false,pre3C: "opcion C", opcionD: false,pre3D: "opcion D" },
+	preg4B={pregunta:"esta es la pregunta 4 B?",opcionA: true,pre4A: "opcion A", opcionB: false,pre4B: "opcion B", opcionC: false,pre4C: "opcion C", opcionD: false,pre4D: "opcion D" },
+	preg5B={pregunta:"esta es la pregunta 5 B?",opcionA: true,pre5A: "opcion A", opcionB: false,pre5B: "opcion B", opcionC: false,pre5C: "opcion C", opcionD: false,pre5D: "opcion D" };
+
+let preg1C={pregunta:"esta es la pregunta 1 C?",opcionA: true,pre1A: "opcion A", opcionB: false,pre1B: "opcion B", opcionC: false,pre1C: "opcion C", opcionD: false,pre1D: "opcion D" },
+	preg2C={pregunta:"esta es la pregunta 2 C?",opcionA: true,pre2A: "opcion A", opcionB: false,pre2B: "opcion B", opcionC: false,pre2C: "opcion C", opcionD: false,pre2D: "opcion D" },
+	preg3C={pregunta:"esta es la pregunta 3 C?",opcionA: true,pre3A: "opcion A", opcionB: false,pre3B: "opcion B", opcionC: false,pre3C: "opcion C", opcionD: false,pre3D: "opcion D" },
+	preg4C={pregunta:"esta es la pregunta 4 C?",opcionA: true,pre4A: "opcion A", opcionB: false,pre4B: "opcion B", opcionC: false,pre4C: "opcion C", opcionD: false,pre4D: "opcion D" },
+	preg5C={pregunta:"esta es la pregunta 5 C?",opcionA: true,pre5A: "opcion A", opcionB: false,pre5B: "opcion B", opcionC: false,pre5C: "opcion C", opcionD: false,pre5D: "opcion D" };
+
+let preg1D={pregunta:"esta es la pregunta 1 D?",opcionA: true,pre1A: "opcion A", opcionB: false,pre1B: "opcion B", opcionC: false,pre1C: "opcion C", opcionD: false,pre1D: "opcion D" },
+	preg2D={pregunta:"esta es la pregunta 2 D?",opcionA: true,pre2A: "opcion A", opcionB: false,pre2B: "opcion B", opcionC: false,pre2C: "opcion C", opcionD: false,pre2D: "opcion D" },
+	preg3D={pregunta:"esta es la pregunta 3 D?",opcionA: true,pre3A: "opcion A", opcionB: false,pre3B: "opcion B", opcionC: false,pre3C: "opcion C", opcionD: false,pre3D: "opcion D" },
+	preg4D={pregunta:"esta es la pregunta 4 D?",opcionA: true,pre4A: "opcion A", opcionB: false,pre4B: "opcion B", opcionC: false,pre4C: "opcion C", opcionD: false,pre4D: "opcion D" },
+	preg5D={pregunta:"esta es la pregunta 5 D?",opcionA: true,pre5A: "opcion A", opcionB: false,pre5B: "opcion B", opcionC: false,pre5C: "opcion C", opcionD: false,pre5D: "opcion D" };
+
+let preg1E={pregunta:"esta es la pregunta 1 E?",opcionA: true,pre1A: "opcion A", opcionB: false,pre1B: "opcion B", opcionC: false,pre1C: "opcion C", opcionD: false,pre1D: "opcion D" },
+	preg2E={pregunta:"esta es la pregunta 2 E?",opcionA: true,pre2A: "opcion A", opcionB: false,pre2B: "opcion B", opcionC: false,pre2C: "opcion C", opcionD: false,pre2D: "opcion D" },
+	preg3E={pregunta:"esta es la pregunta 3 E?",opcionA: true,pre3A: "opcion A", opcionB: false,pre3B: "opcion B", opcionC: false,pre3C: "opcion C", opcionD: false,pre3D: "opcion D" },
+	preg4E={pregunta:"esta es la pregunta 4 E?",opcionA: true,pre4A: "opcion A", opcionB: false,pre4B: "opcion B", opcionC: false,pre4C: "opcion C", opcionD: false,pre4D: "opcion D" },
+	preg5E={pregunta:"esta es la pregunta 5 E?",opcionA: true,pre5A: "opcion A", opcionB: false,pre5B: "opcion B", opcionC: false,pre5C: "opcion C", opcionD: false,pre5D: "opcion D" };
+
 
 const obtenerPreguntas= (categoría)=>{
 	categorías={  //lista de categorias segun su nivel de pregunta
 		categoríaA:[preg1A,preg2A,preg3A,preg4A,preg5A],
-		categoríaB:[preg1B={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg2B={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg3B={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg4B={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg5B={opcionA: true, opcionB: false, opcionC: false, opcionD: false }],
-		categoríaC:[preg1C={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg2C={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg3C={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg4C={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg5C={opcionA: true, opcionB: false, opcionC: false, opcionD: false }],
-		categoríaD:[preg1D={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg2D={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg3D={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg4D={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg5D={opcionA: true, opcionB: false, opcionC: false, opcionD: false }],
-		categoríaE:[preg1E={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg2E={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg3E={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg4E={opcionA: true, opcionB: false, opcionC: false, opcionD: false },preg5E={opcionA: true, opcionB: false, opcionC: false, opcionD: false }]
-	}
+		categoríaB:[preg1B,preg2B,preg3B,preg4B,preg5B],
+		categoríaC:[preg1C,preg2C,preg3C,preg4C,preg5C],
+		categoríaD:[preg1D,preg2D,preg3D,preg4D,preg5D],
+		categoríaE:[preg1E,preg2E,preg3E,preg4E,preg5E]	}
 	if(categorías[categoría]!==undefined){return categorías[categoría];}
 	else{ return false;}
 }
@@ -40,28 +64,21 @@ let orden = [1,2,3,4,5];
 /*--------------------------------------------------------------------------------*/
 
 
-// for (i = 1; i < 6; i++) {  //recorro todos los niveles
-//   orden = orden.sort(function() {return Math.random() - 0.5});
-let i=3;
+for (i = 1; i < 6; i++) {  //recorro todos los niveles
+  orden = orden.sort(function() {return Math.random() - 0.5});
+// let i=3;
   console.log("nivel ",i);
   orden.forEach(function(a) {respuesta=eligeCategoria(a,i);console.log(respuesta);}); //obtengo las preguntas de todas las categorias segun el orden ya elegido por sistema
-// } 
+} 
 
 // respuesta=eligeCategoria(1,1);console.log(respuesta);
 
 
-let empersonas = {
-	firstName:'rubius',
-	lastName:'miguel',
-	otrapersonaA:{firstNam:'yo',lastNam:'si'},
-	otrapersonaB:{firstNam:'tu',lastNam:'no'}
-};
-
-console.log(empersonas.firstName,empersonas.otrapersonaB.firstNam,empersonas.otrapersonaA.lastNam);
-
 
 /*3. El Jugador selecciona una opción de las 4 opciones que tiene, si pierde se finaliza el
 juego si gana continua a la siguiente ronda.*/
+
+
 
 
 /*4. La siguiente ronda selecciona una pregunta de un grado de complejidad mayor según
