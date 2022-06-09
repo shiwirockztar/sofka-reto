7ed0dc4f2bf265e4e781b0e46f66f82e70357784
shiwirockztar <shiwirockztar@gmail.com>
2022-06-09 00:31:48 -0500

    end second day

--- js/script.js
+++ js/script.js
@@ -2,29 +2,29 @@ console.log("start project");
 /*1. Precondiciones: Debe de tener 25 preguntas (5 preguntas por categorías) para 5
 rondas, cada categoría tiene una complejidad o nivel de dificultad, cada ronda debe
 asignarle un premio que el jugador va a ganar, el premio puede ser puntos o dinero.*/
-let preg1A={pregunta:"esta es la pregunta 1 A?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg2A={pregunta:"esta es la pregunta 2 A?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg3A={pregunta:"esta es la pregunta 3 A?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg4A={pregunta:"esta es la pregunta 4 A?",opciones:{A: true,B: false,C: false,D: true},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg5A={pregunta:"esta es la pregunta 5 A?",opciones:{A: true,B: false,C: false,D: true},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
+let preg1A={pregunta:"Según el refran, quien es ciego?",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "La esperanza", B: "El amor", C: "El odio", D: "La envidia" }},
+	preg2A={pregunta:"Uno de los libros de J. K. Rowling es Harry Potter y:",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "El arca perdida", B: "El prisionero de Azkaban", C: "La magia negra", D: "Su varita" }},
+	preg3A={pregunta:"¿Qué trilogía de las películas basadas en los cómics de Marvel recaudó más dinero en taquilla?",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "Thor", B: "Capitán América", C: "Iron Man", D: "Wolverine" }},
+	preg4A={pregunta:"¿Quién es el creador de contenido con más seguidores en la plataforma Twitch?",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "Rubius", B: "Auronplay", C: "Ninja", D: "Ibai" }},
+	preg5A={pregunta:`Quien dirigió la película "El Padrino" en 1972?`,opciones:{A: false,B: false,C: true,D: false},respuestas:{A: "Roman Polanski", B: "Franco Zeffirelli", C: "Francis Ford Copolla", D: "Federico Fellini" }},
 
-  preg1B={pregunta:"esta es la pregunta 1 B?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg2B={pregunta:"esta es la pregunta 2 B?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg3B={pregunta:"esta es la pregunta 3 B?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg4B={pregunta:"esta es la pregunta 4 B?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg5B={pregunta:"esta es la pregunta 5 B?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
+  preg1B={pregunta:"¿Quién era el Dios romano de la guerra?",opciones:{A: false,B: false,C: true,D: false},respuestas:{A: "Ares", B: "Júpiter", C: "Marte", D: "Miércoles" }},
+	preg2B={pregunta:"¿Como se llama el libro sagrado de la cultura Islámica?",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "Talmud", B: "Torá", C: "Corán", D: "Kojiki" }},
+	preg3B={pregunta:"El origen del café proviene de...",opciones:{A: false,B: false,C: true,D: false},respuestas:{A: "Etiopía", B: "Francia", C: "Egipto", D: "Indonesia" }},
+	preg4B={pregunta:"¿Qué enfermedad tenía el legendario astrofísico Stephen Hawking?",opciones:{A: false,B: false,C: true,D: false},respuestas:{A: "Enfermedad de Addison", B: "Progeria de Hutchinson-Gilford", C: "Esclerosis Lateral Amiotrófica", D: "Síndrome de Marfan" }},
+	preg5B={pregunta:"La vara que suele llevar como cetro el Dios de la mitología romana Baco se llama:",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "Tirso", B: "Talión", C: "Vareta", D: "Mástil" }},
 
-  preg1C={pregunta:"esta es la pregunta 1 C?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg2C={pregunta:"esta es la pregunta 2 C?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg3C={pregunta:"esta es la pregunta 3 C?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg4C={pregunta:"esta es la pregunta 4 C?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg5C={pregunta:"esta es la pregunta 5 C?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
+  preg1C={pregunta:"¿Cuál es el océano más grande del mundo?",opciones:{A: false,B: false,C: true,D: false},respuestas:{A: "Océano Pacífico", B: "Océano Índico", C: "Océano Antártico", D: "Océano Atlántico" }},
+	preg2C={pregunta:"Cual de los siguientes países limita con Francia?",opciones:{A: false,B: false,C: false,D: true},respuestas:{A: "Holanda", B: "Suecia", C: "Dinamarca", D: "Belgica" }},
+	preg3C={pregunta:"Cual es el idioma oficial de Israel?",opciones:{A: false,B: false,C: true,D: false},respuestas:{A: "Japones", B: "Griego", C: "Hebreo", D: "Chino" }},
+	preg4C={pregunta:"Jose Tomas Boves murio en la Batalla de:",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "Carabobo", B: "Urica", C: "Mucuritas", D: "Calabozo" }},
+	preg5C={pregunta:"¿Cuál es la capital de Mongolia?",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "Makati City", B: "Ulaanbaatar", C: "Naypyidaw", D: "Nur-Sultan" }},
 
-  preg1D={pregunta:"esta es la pregunta 1 D?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg2D={pregunta:"esta es la pregunta 2 D?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg3D={pregunta:"esta es la pregunta 3 D?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg4D={pregunta:"esta es la pregunta 4 D?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
-	preg5D={pregunta:"esta es la pregunta 5 D?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
+  preg1D={pregunta:"En el aparato reproductor femenino se encuentran las trompas de:",opciones:{A: false,B: false,C: false,D: true},respuestas:{A: "Eustaquio", B: "Diógenes", C: "Augusto", D: "Falopio" }},
+	preg2D={pregunta:"¿Cuál es el hueso mas largo del ser humano?",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "falange", B: "femur", C: "radio", D: "craneo" }},
+	preg3D={pregunta:"El albinismo se presenta por la carencia de:",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "Calcio", B: "Pigmentación", C: "Vitamina A", D: "Oxígeno" }},
+	preg4D={pregunta:"Son las membranas movibles cubiertas de piel que resguardan los ojos:",opciones:{A: false,B: false,C: true,D: false},respuestas:{A: "Cejas", B: "Pupilas", C: "Parpados", D: "Anteojos" }},
+	preg5D={pregunta:"¿Qué parte del cuerpo produce insulina?",opciones:{A: false,B: true,C: false,D: false},respuestas:{A: "El páncreas", B: "El bazo", C: "El hígado", D: "El timo" }},
 
   preg1E={pregunta:"esta es la pregunta 1 E?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
 	preg2E={pregunta:"esta es la pregunta 2 E?",opciones:{A: true,B: false,C: false,D: false},respuestas:{A: "opcion A", B: "opcion B", C: "opcion C", D: "opcion D" }},
@@ -35,11 +35,11 @@ let preg1A={pregunta:"esta es la pregunta 1 A?",opciones:{A: true,B: false,C: fa
 
 const obtenerPreguntas= (categoría)=>{
 	categorías={  //lista de categorias segun su nivel de pregunta
-		categoríaA:[preg1A,preg2A,preg3A,preg4A,preg5A],
-		categoríaB:[preg1B,preg2B,preg3B,preg4B,preg5B],
-		categoríaC:[preg1C,preg2C,preg3C,preg4C,preg5C],
-		categoríaD:[preg1D,preg2D,preg3D,preg4D,preg5D],
-		categoríaE:[preg1E,preg2E,preg3E,preg4E,preg5E]	}
+		social:[preg1A,preg2A,preg3A,preg4A,preg5A],
+		historia:[preg1B,preg2B,preg3B,preg4B,preg5B],
+		geografia:[preg1C,preg2C,preg3C,preg4C,preg5C],
+		anatomia:[preg1D,preg2D,preg3D,preg4D,preg5D],
+		ciencia:[preg1E,preg2E,preg3E,preg4E,preg5E]	}
 	if(categorías[categoría]!==undefined){return categorías[categoría];}
 	else{ return false;}
 }
@@ -55,11 +55,11 @@ const mostrarPregunta=(objeto)=>{
 
 const eligeCategoria=(n,nivel)=>{
 	let rta;
-	if (n==1) {rta=obtenerPreguntas("categoríaA");}
-	if (n==2) {rta=obtenerPreguntas("categoríaB");}
-	if (n==3) {rta=obtenerPreguntas("categoríaC");}
-	if (n==4) {rta=obtenerPreguntas("categoríaD");}
-	if (n==5) {rta=obtenerPreguntas("categoríaE");}
+	if (n==1) {rta=obtenerPreguntas("social");}
+	if (n==2) {rta=obtenerPreguntas("historia");}
+	if (n==3) {rta=obtenerPreguntas("geografia");}
+	if (n==4) {rta=obtenerPreguntas("anatomia");}
+	if (n==5) {rta=obtenerPreguntas("ciencia");}
 	nivel--; //el nivel esta -1 por indice de arreglo
 	// mostrarPregunta(rta[nivel]);
 	return rta[nivel]; //regresa la pregunta
